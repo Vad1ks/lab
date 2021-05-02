5e017ba1b661628ce62c5d971d368bd99d84813f
vadik <vadiksosnovenko@gmail.com>
2021-05-02 22:06:21 +0300

    added tests for class Group

--- test_main.py
+++ test_main.py
@@ -1,5 +1,7 @@
 from main import *
 import pytest
+import random
+import statistics
 
 
 def test_user_count():
@@ -31,3 +33,23 @@ def test_averageMark_student(test_input, expected):
     st = Student()
     st.add_marks(*test_input)
     assert st.get_average_mark() == expected
+
+
+def test_addStudents_to_group():
+    testStudentsGroup = (Student("Vadym"), Student("Viktor"), Student("Olha"))
+    g = Group("A")
+    g.add_student(*testStudentsGroup)
+    assert g.get_students_count() == 3
+
+
+def test_averageMark_group():
+    g = Group("B")
+    marklist = []
+    for i in range(5):
+        s = Student()
+        for j in range(5):
+            mark = random.randint(1, 5)
+            marklist.append(mark)
+            s.add_marks(mark)
+        g.add_student(s)
+    assert statistics.mean(marklist) == g.get_average_mark()
