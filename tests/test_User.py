b163772ccd96733de6d33d03bc35ed6db0fa9b87
vadik <vadiksosnovenko@gmail.com>
2021-05-10 20:44:27 +0300

    minor fix

--- tests/test_User.py
+++ tests/test_User.py
@@ -4,14 +4,15 @@ import pytest
 @pytest.mark.parametrize("test_input, expected",
                          [
                              (3, 3),
-                             (4, 7),
-                             (0, 7),
+                             (4, 4),
+                             (0, 0),
                              pytest.param(4, 5, marks=pytest.mark.xfail)
                          ]
                          )
 def test_user_count(test_input, expected):
     [User() for _ in range(test_input)]
     assert User.get_all_users_count() == expected
+    User.users = []
 
 def test_write_message():
     u = User()
