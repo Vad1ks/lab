e285d32d3b9497e6aad97cb8437821b8ad379aae
vadik <vadiksosnovenko@gmail.com>
2021-05-10 20:10:17 +0300

    added test for writing messages

--- tests/test_User.py
+++ tests/test_User.py
@@ -1,4 +1,4 @@
-from main import User
+from main import User, Chat
 import pytest
 
 @pytest.mark.parametrize("test_input, expected",
@@ -12,3 +12,9 @@ import pytest
 def test_user_count(test_input, expected):
     [User() for _ in range(test_input)]
     assert User.get_all_users_count() == expected
+
+def test_write_message():
+    u = User()
+    c = Chat("test")
+    u.write_message("test", "Hello")
+    assert len(c.messages) == 1
