bdb72fae0b0562af9485936ba5d93093ce32586c
vadik <vadiksosnovenko@gmail.com>
2021-05-10 20:52:48 +0300

    added test for chat count

--- tests/test_Chat.py
+++ tests/test_Chat.py
@@ -6,10 +6,26 @@ import pytest
                              ((Student(), Student(), Student(), Professor()), 4),
                              ((Student(), Professor()), 2),
                              ((), 0),
-                             pytest.param(Student(), 2, marks=pytest.mark.xfail)
+                             pytest.param(Student(), 2, marks=pytest.mark.xfail),
+                             pytest.param((Student(), Student()), 3, marks=pytest.mark.xfail)
                          ]
                          )
 def test_addUser_chat(input_array_of_users, expected):
     c = Chat("C")
     c.add_user(*input_array_of_users)
     assert len(c.users) == expected
+
+
+@pytest.mark.parametrize("test_input, expected",
+                         [
+                             (3, 3),
+                             (4, 4),
+                             (0, 0),
+                             pytest.param(4, 5, marks=pytest.mark.xfail)
+                         ]
+                         )
+def test_chat_count(test_input, expected):
+    Chat.chatList = []
+    [Chat("test") for _ in range(test_input)]
+    assert len(Chat.chatList) == expected
+
